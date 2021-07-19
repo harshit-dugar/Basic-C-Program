7296de340f485a11fd3d716223604f653f2af4f8
harshit-dugar <dugar87711@gmail.com>
2021-07-19 09:59:20 +0530

    call by reference

---
+++ swap_byreference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap(int *a,int *b)
+{
+    int temp;
+    temp =*a;
+    *a = *b;
+    *b = temp;
+    printf("After swapping %d %d\n",*a,*b);
+}
+int main()
+{
+    int c,d;
+    printf("Enter two numbers");
+    scanf("%d %d",&c,&d);
+
+    swap(&c,&d);
+
+    return 0;
+}
